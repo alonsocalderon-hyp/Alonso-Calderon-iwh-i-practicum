d52535de504ce08eb7d1d4aab9a6c01d58a4c21c
Alonso Calderon <alonso.calderon@hyperscayle.com>
2026-05-25 13:58:05 -0700

    Headers and labels for columns

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ app.use(express.static('public'));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+const HUBSPOT_TOKEN = process.env.PRIVATE_APP_ACCESS_TOKEN;
+
+//Object API Name
+const OBJECT_TYPE = '2-63084340';
+
+//Column API name
+const PROPERTY_1 = 'pet_name';           
+const PROPERTY_2 = 'color';   
+const PROPERTY_3 = 'pet_type';
+const PROPERTY_4 = 'sound';
+
+// Column headers 
+const COL_1_LABEL = 'Name';
+const COL_2_LABEL = 'Color';
+const COL_3_LABEL = 'Type';
+const COL_4_LABEL = 'Sound';
+
+
