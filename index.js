01c0433d30ad00523f95d9098a8bd6fc0a224260
Alonso Calderon <alonso.calderon@hyperscayle.com>
2026-05-25 12:22:58 -0700

    Configure Express

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
+// Configure express for parsing, finding files and use Pug
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
